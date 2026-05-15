381bf09e006d228cb304879af2125a787af6ed57
moises-projects1220 <Tradermoises1220@gmail.com>
2026-05-15 00:22:39 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,8 @@
-"use client";
-
-import { Header } from "@/components/layout/Header";
-import { LeftSidebar } from "@/components/layout/LeftSidebar";
-import { RightSidebar } from "@/components/layout/RightSidebar";
-import { BottomPanel } from "@/components/layout/BottomPanel";
-import { PriceChart } from "@/components/chart/PriceChart";
-import { IndicatorSettingsDialog } from "@/components/chart/IndicatorSettingsDialog";
-import { useChartStore } from "@/lib/store/chart-store";
-
-export default function HomePage() {
-  const symbol = useChartStore((s) => s.symbol);
-  const timeframe = useChartStore((s) => s.timeframe);
-
+export default function Home() {
   return (
-    <div className="flex h-screen w-screen flex-col overflow-hidden bg-tv-bg">
-      <Header />
-      <div className="flex min-h-0 flex-1">
-        <LeftSidebar />
-        <main className="relative flex min-h-0 flex-1 flex-col">
-          <div className="min-h-0 flex-1">
-            <PriceChart symbol={symbol} timeframe={timeframe} />
-          </div>
-        </main>
-        <RightSidebar />
-      </div>
-      <BottomPanel />
-      <IndicatorSettingsDialog />
-    </div>
+    <iframe
+      src="/chart.html"
+      style={{ width: '100vw', height: '100vh', border: 'none' }}
+    />
   );
 }
